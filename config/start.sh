97eada9062b094b0e71ad20d138faa57da37fe21
Rick Barber <barber5@stanford.edu>
2014-01-08 13:29:59 -0800

    no message

--- config/start.sh
+++ config/start.sh
@@ -1,1 +1,1 @@
-mvn archetype:generate -DgroupId=co.brbr5.app -DartifactId=my-app -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false --settings settings.xml
+mvn archetype:generate -DgroupId=co.brbr5.app -DartifactId=my-app -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false --settings config/settings.xml
