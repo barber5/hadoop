243f822b17ad4e3eeb1bfc15a41bc9562dd03b6d
Rick Barber <barber5@stanford.edu>
2014-01-10 23:19:20 -0800

    no message

--- FriendRec.java
+++ FriendRec.java
@@ -60,6 +60,7 @@ public class FriendRec extends Configured implements Tool {
       @Override
       public void map(LongWritable key, Text value, Context context)
               throws IOException, InterruptedException {
+         System.out.println(value.toString());
          int user = Integer.parseInt(value.toString().split("\t")[0]);
          String[] friendsStr = value.toString().split("\t")[1].split(",");
          for(String friendiStr: friendsStr) {
