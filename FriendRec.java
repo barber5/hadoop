c3862a3aa2664ff60c881fd787887afe90615160
Rick Barber <barber5@stanford.edu>
2014-01-12 11:54:46 -0800

    no message

--- FriendRec.java
+++ FriendRec.java
@@ -33,7 +33,7 @@ public class FriendRec extends Configured implements Tool {
       Job job = new Job(getConf(), "FriendRec");
       job.setJarByClass(FriendRec.class);
       job.setOutputKeyClass(IntWritable.class);
-      job.setOutputValueClass(IntWritable.class);
+      job.setOutputValueClass(Iterable.class);
 
       job.setMapperClass(Map.class);
       job.setReducerClass(Reduce.class);
