71bc2eef8cc185a919ccde568b2c12962437e34b
Rick Barber <barber5@stanford.edu>
2014-01-12 11:23:39 -0800

    no message

--- FriendRec.java
+++ FriendRec.java
@@ -103,7 +103,7 @@ public class FriendRec extends Configured implements Tool {
               throws IOException, InterruptedException {
          HashMap<Integer, FriendCount> counts = new HashMap<Integer, FriendCount>();
          HashMap<Integer, Boolean> ignoreList = new HashMap<Integer, Boolean>(); // I hate java
-         for(TupleWritable tw: values) { // count our mutual friends
+         for(Iterable<IntWritable> tw: values) { // count our mutual friends
             IntWritable candWrite = (IntWritable) tw.get(0);
             IntWritable candCount = (IntWritable) tw.get(1);
             int candidate = candWrite.get();
