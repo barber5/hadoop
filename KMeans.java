0dac783e463df9634b6dad37a037674a260542aa
Rick Barber <barber5@stanford.edu>
2014-02-05 13:54:05 -0800

    no message

--- KMeans.java
+++ KMeans.java
@@ -7,18 +7,20 @@ import java.util.Arrays;
 import java.util.UUID;
 import java.util.Vector;
 
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.filecache.DistributedCache;
-import org.apache.hadoop.fs.*;
-import org.apache.hadoop.io.*;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
 
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
@@ -35,15 +37,21 @@ public class KMeans extends Configured implements Tool {
         System.out.println(Arrays.toString(args));
         Configuration conf = getConf();
 
-        Job job = new Job(conf, "FriendRec");
+        Job job = new Job(getConf(), "FriendRec");
         job.setJarByClass(KMeans.class);
         job.setOutputKeyClass(IntWritable.class);
         job.setOutputValueClass(DoubleArrayWritable.class);
+
         job.setMapperClass(Map.class);
         job.setReducerClass(Reduce.class);
-        job.setInputFormatClass(TextInputFormat.class); // breaks into lines
-        job.setOutputFormatClass(TextOutputFormat.class);
 
+        job.setInputFormatClass(TextInputFormat.class); // breaks into lines
+        job.setOutputFormatClass(org.apache.hadoop.mapreduce.lib.output.TextOutputFormat.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.waitForCompletion(true);
 
 
 
