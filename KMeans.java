6c6de905e7721a4c76578d3899c1fd23e773bc16
Rick Barber <barber5@stanford.edu>
2014-02-07 21:24:31 -0800

    no message

--- KMeans.java
+++ KMeans.java
@@ -183,7 +183,7 @@ public class KMeans extends Configured implements Tool {
             cost += finalDist;
             DoubleArrayWritable v = new DoubleArrayWritable(vec);
             DoubleArrayWritable k = new DoubleArrayWritable(centroid);
-            System.out.println("With cost "+closest+" best centroid is "+centroid);
+            //System.out.println("With cost "+closest+" best centroid is "+centroid);
             context.write(k, v);
         }
         @Override
